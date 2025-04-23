dc44372abba1ebab7ce311fcd596f3b4932a6a4b
Saurabh Lodha <saurabhlodha11@gmail.com>
2025-04-23 15:48:55 +0200

    Update workflow image, use PYPPETEER to render image

--- src/tools/draw_graph.py
+++ src/tools/draw_graph.py
@@ -3,5 +3,5 @@ from langchain_core.runnables.graph import MermaidDrawMethod
 def draw_workflow_image(app):
   app.get_graph().draw_mermaid_png(
     output_file_path="workflow.png",
-    draw_method=MermaidDrawMethod.API,
+    draw_method=MermaidDrawMethod.PYPPETEER,
   )
